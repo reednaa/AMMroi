45293afda3140f43864129062c8675dd9448084d
Lindgren <github@lindgren.xyz>
2020-10-29 17:29:07 +0100

    new chart

--- js/index.js
+++ js/index.js
@@ -9,8 +9,24 @@ function fetchDataForPair(pair) {
 }
 console.log(fetchDataForPair("wbtc"));
 
-function getFeesFromData(json, start_date, end_date) {
-
+function getROIFromData(json_data, start_date) {
+    if (start_date) {
+        start_date = moment.unix(1);
+    }
+    let index_data = json_data[0].indexOf("ROI");
+    let index_time = json_data[0].indexOf("Timestamp");
+    let start_index;
+    for (arr in json_data.slice(1)) {
+        if (moment.unix(json_data[arr][index_time]) >= start_date) {
+            start_index = arr;
+            break;
+        }
+    }
+    output_data = [];
+    for (arr of json_data.slice(start_index)) {
+        output_data.push({x: arr[index_data]/arr[start_index], y: moment.unix(arr[index_time])});
+    }
+    return output_data;
 }
 
 function getILFromData(json, start_date, end_date) {
@@ -22,19 +38,21 @@ var ctx = document.getElementById('mainChart').getContext('2d');
 var mixedChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        datasets: [{
-            label: 'Bar Dataset',
-            data: [10, 20, 30, 40],
-            // this dataset is drawn below
-            order: 1
-        }, {
+        datasets: [
+        //     {
+        //     label: 'Bar Dataset',
+        //     data: [10, 20, 30, 40],
+        //     // this dataset is drawn below
+        //     order: 1
+        // }, 
+        {
             label: 'Line Dataset',
-            data: [10, 10, 10, 10],
+            data: getROIFromData("wbtc", false),
             type: 'line',
             // this dataset is drawn on top
             order: 2
         }],
-        labels: ['January', 'February', 'March', 'April']
+        //labels: ['January', 'February', 'March', 'April']
     },
 });
 
