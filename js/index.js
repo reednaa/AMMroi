f54dc1cca4be54d129d1fdad61d54ca72425bfed
Lindgren <github@lindgren.xyz>
2020-11-01 20:44:16 +0100

    Update everything

--- js/index.js
+++ js/index.js
@@ -24,11 +24,12 @@ function fetchAllTokens() {
 }
 // console.log(fetchDataForPair("wbtc"));
 
-function getROIFromData(json_data, start_date) {
-    if (start_date) {
+function convertData(json_data, start_date) {
+    if (!start_date) {
         start_date = moment.unix(1);
     }
-    let index_data = json_data[0].indexOf("ROI");
+    let index_ROI = json_data[0].indexOf("ROI");
+    let index_TP = json_data[0].indexOf("Token Price");
     let index_time = json_data[0].indexOf("timestamp");
     let start_index;
     for (arr_index in json_data.slice(1)) {
@@ -37,44 +38,38 @@ function getROIFromData(json_data, start_date) {
             break;
         }
     }
-    output_data = [];
-    const initialInv = json_data[start_index][index_data];
+    output_Fees = [];
+    const initialInv = json_data[start_index][index_ROI];
     for (arr of json_data.slice(start_index)) {
-        output_data.push(
+        output_Fees.push(
             {
             x: moment.unix(arr[index_time]),
-            y: arr[index_data]/initialInv
+            y: arr[index_ROI]/initialInv
             }
         );
     }
-    return output_data;
+    output_TP = [];
+    const intialPrice = json_data[start_index][index_TP];
+    for (arr of json_data.slice(start_index)) {
+        output_TP.push(
+            {
+            x: moment.unix(arr[index_time]),
+            y: 2 * Math.sqrt(arr[index_TP]/intialPrice)/(1 + arr[index_TP]/intialPrice)
+            }
+        );
+    }
+    output_ROI = [];
+    for (arr of json_data.slice(start_index)) {
+        output_ROI.push(
+            {
+            x: moment.unix(arr[index_time]),
+            y: (arr[index_ROI]/initialInv) * (2 * Math.sqrt(arr[index_TP]/intialPrice)/(1 + arr[index_TP]/intialPrice))
+            }
+        );
+    }
+    return [output_fees, output_TP, output_ROI]
 }
 
-function getILFromData(json_data, start_date) {
-    if (start_date) {
-        start_date = moment.unix(1);
-    }
-    let index_data = json_data[0].indexOf("Token Price");
-    let index_time = json_data[0].indexOf("timestamp");
-    let start_index;
-    for (arr_index in json_data.slice(1)) {
-        if (moment.unix(json_data[arr_index][index_time]) >= start_date) {
-            start_index = arr_index+1;
-            break;
-        }
-    }
-    output_data = [];
-    const intialPrice = json_data[start_index][index_data];
-    for (arr of json_data.slice(start_index)) {
-        output_data.push(
-            {
-            x: moment.unix(arr[index_time]),
-            y: 2 * Math.sqrt(arr[index_data]/intialPrice)/(1 + arr[index_data]/intialPrice)
-            }
-        );
-    }
-    return output_data;
-}
 
 function createChart(ROIdata, ILdata) {
     var ctx = document.getElementById('mainChart').getContext('2d');
@@ -125,7 +120,102 @@ function removeData(chart) {
 
 // var chart = createChart()
 
-fetchDataForPair("wbtc");
+// fetchDataForPair("wbtc");
+
+function dictSearch(arr, key, value) {
+    for (element of arr) {
+        if (element[key] == value) {
+            return arr.indexOf(element);
+        }
+    }
+}
 
 
+let app = new Vue({
+    el: "#structure",
+    data: {
+        charts: [],
+        list_of_tokens: [],
+        selectedAsset: false,
+    },
+    methods: {
+        addChart: function() {
+            let name = this.charts.length
+            let new_chart = {name: name};
+            this.charts.push(new_chart);
+            setTimeout(function() {
+                var ctx = document.getElementById(name).getContext('2d');
+                let chart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        datasets: [
+                        //     {
+                        //     label: 'ROI',
+                        //     data: ROIdata
+                        // },
+                        // {
+                        //     label: 'IL',
+                        //     data: ILdata
+                        // }
+                    ]
+                    },
+                    options: {
+                        scales: {
+                            xAxes: [{
+                                type: "time",
+                                time : {
+                                    tooltipFormat: 'll HH:mm'
+                                },
+                                // ticks: {
+                                //     min: ROIdata[0]["x"]
+                                // }
+                            }]
+                        }
+                    }
+                });
+                new_chart["chart"] = chart;
+                Vue.set(app.charts, dictSearch(app.charts, "name", name), new_chart);
+                app.addData(chart, app.selectedAsset, false);
+            }, 500);
+        },
+        addData: function(chart, currency, start_time) {
+            if (!start_date) {
+                start_date = moment.unix(1);
+            }
+            Papa.parse("/AMMroi/data/uniswapv2/roi/" + currency + ".csv", {
+                download: true,
+                complete: function(results) {
+                    output_fees, output_TP, output_ROI = convertData(results, start_time)
+                    chart.datasets.push(
+                        {
+                            label: "Collected Fees",
+                            data: output_fees
+                        }
+                    );
+                    chart.datasets.push(
+                        {
+                            label: "Percentage against HODL",
+                            data: output_TP
+                        }
+                    );
+                    chart.datasets.push(
+                        {
+                            label: "Return",
+                            data: output_ROI
+                        }
+                    );
+                }
+            });
+        },
+        fetchAllTokens: function() {
+        let url = '/AMMroi/data/uniswapv2/tokens.json';
+        fetch(url).then(res => res.json()).then((out) => {
+            app.list_of_tokens = out;
+        });
+}
+    },
+    created() {
+        fetchAllTokens();
+    }
+});
 
