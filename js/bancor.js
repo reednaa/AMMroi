3b3aeab826973602fa62ad930bf092e8cf8b865d
Lindgren <github@lindgren.xyz>
2020-11-28 11:53:16 +0100

    created

--- js/bancor.js
+++ js/bancor.js
@@ -59,7 +59,7 @@ let app = new Vue({
         parsedProtections: [],
         parsedProtectionInc: 0,
         protectionMaxID: 1000,
-        translator: {0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE: "ETH", 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2: "MKR"}
+        translator: {"0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE": "ETH", "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2": "MKR"}
     },
     methods: {
         setProvider: function() {
@@ -97,5 +97,8 @@ let app = new Vue({
             app.sortProtections();
         }
         }
+    },
+    created() {
+        app.setProvider();
     }
 });
