4b59a23f50a4cc62bdb3bcd64479a7a6298d2b0f
Lindgren <github@lindgren.xyz>
2020-12-09 13:42:56 +0100

    18 minus

--- js/bancor.js
+++ js/bancor.js
@@ -55,9 +55,9 @@ async function parseProtections() {
                     Vue.set(app.parsedProtections, protection, {decimals: value, rate: pp[6]/pp[5], ...app.parsedProtections[protection]});
                 } else {
                     if (pp[2] == "0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c") {  // Then this is BNT
-                        Vue.set(app.parsedProtections, protection, {decimals: value, rate: pp[6]/pp[5]/10**value, ...app.parsedProtections[protection]});
+                        Vue.set(app.parsedProtections, protection, {decimals: value, rate: pp[6]/pp[5]/10**(18-value), ...app.parsedProtections[protection]});
                     } else {
-                        Vue.set(app.parsedProtections, protection, {decimals: value, rate: (pp[6]/pp[5])*10**value, ...app.parsedProtections[protection]});
+                        Vue.set(app.parsedProtections, protection, {decimals: value, rate: (pp[6]/pp[5])*10**(18-value), ...app.parsedProtections[protection]});
                     }
                 }
                 
