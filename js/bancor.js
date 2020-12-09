216e77ccd41b98633d9a907878bee1d5d0d9d015
Lindgren <github@lindgren.xyz>
2020-12-09 14:08:39 +0100

    const opposite_token

--- js/bancor.js
+++ js/bancor.js
@@ -72,6 +72,7 @@ async function parseProtections() {
                         if (app.decimals(opposite_token)) {
                             Vue.set(app.parsedProtections, protection, {decimals: app.decimals[pp[2]], rate: pp[6]/pp[5]/10**(18-app.decimals[opposite_token]), ...app.parsedProtections[protection]});
                         } else {
+                            const opposite_token = app.translator(pp[1]).replace("BNT", "");
                             const OT = new app.web3.eth.Contract(ERC20, opposite_token);
                             await OT.methods.decimals().call().then(function(value2) {
                                 Vue.set(app.decimals, pp[1], value2);
